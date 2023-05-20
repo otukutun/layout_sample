ed016179eaea888e6769e8c61e717e8fa1c0c69b
otukutun <otukutun@gmail.com>
2023-05-21 00:23:28 +0900

    Add images section.

--- lib/main.dart
+++ lib/main.dart
@@ -107,6 +107,19 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -124,7 +137,13 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Column(
-        children: [titleSection, buttonSection],
+        children: [
+          Image.asset('images/lake.jpg',
+              width: 600, height: 240, fit: BoxFit.cover),
+          titleSection,
+          buttonSection,
+          textSection
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
